734274bcc0409f6d12d5de2e435fc60312c2f200
Manuel Kienlein <manuel.kienlein@gmx.de>
2023-11-18 17:06:57 +0100

    Add recipe details to admin view

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Ingredient
 from .models import Recipe 
 
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "difficulty", "duration", "image_id"]
+
+
 admin.site.register(Ingredient)
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeAdmin)
 
