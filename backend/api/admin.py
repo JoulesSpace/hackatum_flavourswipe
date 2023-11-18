d81ce88d086e0f956c732c200dc4096ad33a80e3
Manuel Kienlein <manuel.kienlein@gmx.de>
2023-11-18 23:22:49 +0100

    Improve admin ui for user feedback

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -7,8 +7,12 @@ class RecipeAdmin(admin.ModelAdmin):
     list_display = ["name", "description", "difficulty", "duration", "image_id"]
 
 
+class UserFeedbackAdmin(admin.ModelAdmin):
+    list_display = ["user", "recipe", "feedback"]
+
+
 admin.site.register(Ingredient)
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(UserFeedback)
+admin.site.register(UserFeedback, UserFeedbackAdmin)
 
 
