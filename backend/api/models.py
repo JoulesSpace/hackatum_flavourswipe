4d1d636f31a9e8e8ed90c604cc49a98caf513cde
Manuel Kienlein <manuel.kienlein@gmx.de>
2023-11-18 09:42:32 +0100

    Added simple database model

---
+++ backend/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=512)
+    difficulty = models.IntegerField()
+    duration = models.IntegerField()
+    ingredients = models.ManyToManyField(Ingredient)
+    image_id = models.CharField(max_length=128)
