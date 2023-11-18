8f0882bfa494034445deb21f02132f794cccfab6
Manuel Kienlein <manuel.kienlein@gmx.de>
2023-11-18 20:52:33 +0100

    Added api and model for user feedback

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from api import views
+from .views import LikeView, DislikeView
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -13,5 +14,7 @@ from . import views
 urlpatterns = [
     #path("", views.index, name="index"),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('like/<int:pk>/', LikeView.as_view(), name='like'),
+    path('dislike/<int:pk>/', DislikeView.as_view(), name='dislike'),
 ]
