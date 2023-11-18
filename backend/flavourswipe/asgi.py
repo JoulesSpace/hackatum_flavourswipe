d4727078ab12e4d1f1dfb1c41f87bfa84593d009
Manuel Kienlein <manuel.kienlein@gmx.de>
2023-11-18 10:57:39 +0100

    Moved django

--- backend/flavourswipe/asgi.py
+++ backend/flavourswipe/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for FlavourSwipe project.
+ASGI config for flavourswipe project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core import get_asgi_application
+from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FlavourSwipe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flavourswipe.settings')
 
 application = get_asgi_application()
